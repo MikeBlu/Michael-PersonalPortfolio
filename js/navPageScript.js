5191d8e1aae34d0beb70929044dcead4e1794f55
Michael Gaming <compmichaelb90@gmail.com>
2024-10-23 16:38:21 +0400

    Fixed underlying mobile issues (portrait).

--- js/navPageScript.js
+++ js/navPageScript.js
@@ -1,70 +1,82 @@
 let menuShown = false;
 
-    window.onload = () => {
-        const indicator = document.getElementById("cursorIndicator");
+window.onload = () => {
 
-        document.body.onpointermove = event => {
-            const { clientX, clientY } = event;
+    const indicator = document.getElementById("cursorIndicator");
 
-            indicator.animate({
-                left: `${clientX}px`,
-                top: `${clientY}px`
-            
-            }, {duration: 500, fill: "forwards"})
+    document.body.onpointermove = event => {
+        const { clientX, clientY } = event;
 
-            indicator.style.maskPosition = `${clientX}px ${clientY}px`;
+        indicator.animate({
+            left: `${clientX}px`,
+            top: `${clientY}px`
+        
+        }, {duration: 500, fill: "forwards"})
 
+        indicator.style.maskPosition = `${clientX}px ${clientY}px`;
+
+    }
+
+    let navCards = document.querySelectorAll('div[class^="navCard"]');
+
+    function rotateNavCards(open) {
+        for (let i = 0; i < navCards.length; i++ ) {
+            navCards[i].style.visibility = (open)?("hidden"):("visible");
+            navCards[i].style.transform = `rotate(${(open)?(0):(30*(i+1))}deg)`;
         }
+    }
 
-        let navCards = document.querySelectorAll('div[class^="navCard"]');
 
-        function rotateNavCards(open) {
-            for (let i = 0; i < navCards.length; i++ ) {
-                navCards[i].style.visibility = (open)?("hidden"):("visible");
-                navCards[i].style.transform = `rotate(${(open)?(0):(30*(i+1))}deg)`;
-            }
+    const navButton = document.getElementById("navButton");
+    const navIcon = document.getElementById("hamburgerMenu_SVG");
+    navButton.onclick = () => {
+        if (menuShown) {
+            navIcon.childNodes[1].style.transform = "none";
+            navIcon.childNodes[3].style.opacity = "100%";
+            navIcon.childNodes[5].style.transform = "none";
+            rotateNavCards(true);
+            menuShown = false;
+        } else {
+            navIcon.childNodes[1].style.transform = "translate(5px,-22px) rotate(45deg)";
+            navIcon.childNodes[3].style.opacity = "0%";
+            navIcon.childNodes[5].style.transform = "translate(5px,25px) rotate(-45deg)";
+            rotateNavCards(false);
+            menuShown = true;
         }
+    };
 
+    const mapPoints = document.getElementsByClassName("rmap_fill")
+    , timelineTrailBr = document.getElementById("timelineTrailBright"),
+    markerText = document.getElementsByClassName("markerText");
 
-        const navButton = document.getElementById("navButton");
-        const navIcon = document.getElementById("hamburgerMenu_SVG");
-        navButton.onclick = () => {
-            if (menuShown) {
-                navIcon.childNodes[1].style.transform = "none";
-                navIcon.childNodes[3].style.opacity = "100%";
-                navIcon.childNodes[5].style.transform = "none";
-                rotateNavCards(true);
-                menuShown = false;
-            } else {
-                navIcon.childNodes[1].style.transform = "translate(5px,-22px) rotate(45deg)";
-                navIcon.childNodes[3].style.opacity = "0%";
-                navIcon.childNodes[5].style.transform = "translate(5px,25px) rotate(-45deg)";
-                rotateNavCards(false);
-                menuShown = true;
-            }
-        };
-
-        const mapPoints = document.getElementsByClassName("rmap_fill")
-        , timelineTrailBr = document.getElementById("timelineTrailBright"),
-        markerText = document.getElementsByClassName("markerText");
-
-        let mouseEnterEvent = 'ontouchstart' in window ? 'touchstart' : 'mouseenter',
-        mouseLeaveEvent = 'ontouchend' in window ? 'touchend' : 'mouseleave';
-
-        for (let i = 0; i < mapPoints.length; i++) {
-            if ( markerText[i] == undefined) continue;
-            mapPoints[i].addEventListener(mouseEnterEvent, function(e) {
+    for (let i = 0; i < mapPoints.length; i++) {
+        if ( markerText[i] == undefined) continue;
+        mapPoints[i].addEventListener('mouseenter', function(e) {
+            markerText[i].style.visibility ="visible";
+        });
+        mapPoints[i].addEventListener('mouseleave', function(e) {
+            markerText[i].style.visibility ="hidden";
+        });
+        document.addEventListener('touchstart', function checkHover() {
+            if (mapPoints[i].matches(':hover')) {
                 markerText[i].style.visibility ="visible";
-            });
-            mapPoints[i].addEventListener(mouseLeaveEvent, function(e) {
+            } else {
                 markerText[i].style.visibility ="hidden";
-            });
-        }
+            }
+        });
+    }
 
-        mapPoints[mapPoints.length-1].addEventListener(mouseEnterEvent, function(e) {
+    mapPoints[mapPoints.length-1].addEventListener('mouseenter', function(e) {
+        timelineTrailBr.style.opacity = "100%";
+    });
+    mapPoints[mapPoints.length-1].addEventListener('mouseleave', function(e) {
+        timelineTrailBr.style.opacity = "0%";
+    });
+    document.addEventListener('touchstart', function checkHover() {
+        if (mapPoints[mapPoints.length-1].matches(':hover')) {
             timelineTrailBr.style.opacity = "100%";
-        });
-        mapPoints[mapPoints.length-1].addEventListener(mouseLeaveEvent, function(e) {
+        } else {
             timelineTrailBr.style.opacity = "0%";
-        });
+        }
+    });
 };
